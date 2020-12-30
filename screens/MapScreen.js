873ddfb896dab9c255cf438aafba522dc0f8056a
Julie <julienne.bay@gmail.com>
2020-12-30 14:25:26 -0500

    The map becomes 'read only' if the place has already been saved -- in other words, the user can't select another location once the location has been saved and is being viewed on the map screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -11,11 +11,16 @@ import Colors from "../constants/Colors";
 //The MapView package: https://docs.expo.io/versions/v40.0.0/sdk/map-view/
 
 const MapScreen = props => {
-  const [selectedLocation, setSelectedLocation] = useState();
+  //initialLocation & readonly - set in PlaceDetailScreen.js (line 21)
+  //they might be undefined or set (if the location has been saved and the user is accessing the map screen from the PlaceDetailScreen.js)
+  const initialLocation = props.navigation.getParam("initialLocation");
+  const readonly = props.navigation.getParam("readonly");
+
+  const [selectedLocation, setSelectedLocation] = useState(initialLocation);
 
   const mapRegion = {
-    latitude: 45.5,
-    longitude: -73.56,
+    latitude: initialLocation ? initialLocation.lat : 45.5,
+    longitude: initialLocation ? initialLocation.lng : -73.56,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421
   };
@@ -23,6 +28,12 @@ const MapScreen = props => {
   const selectLocationHandler = event => {
     //the "event" here is when the user clicks on the map
     //it returns an object, which includes the coordinates (of the location that the user clicked)
+
+    if (readonly) {
+      //if it's read only (see notes on line 15), then the user can't select another new location
+      return;
+    }
+
     setSelectedLocation({
       lat: event.nativeEvent.coordinate.latitude,
       lng: event.nativeEvent.coordinate.longitude
@@ -72,7 +83,12 @@ const MapScreen = props => {
 
 MapScreen.navigationOptions = navData => {
   const saveFn = navData.navigation.getParam("saveLocation");
-
+  //readonly - see notes on line 15
+  const readonly = props.navigation.getParam("readonly");
+  if (readonly) {
+    //if readonly is set, then we won't show the "save" button
+    return {};
+  }
   return {
     headerRight: (
       <TouchableOpacity style={styles.headerButton} onPress={saveFn}>
